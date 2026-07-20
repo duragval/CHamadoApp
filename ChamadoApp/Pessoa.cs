c96747b666b2220d624f8fbd56da0d775c9c601e
duragval <varleioli@gmail.com>
2026-07-20 20:57:53 -0300

    Primeiro Commit

---
+++ ChamadoApp/Pessoa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace ChamadoApp
+{
+
+    abstract class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, string nascimento, long cpf)
+        {
+            this.Nome = nome;
+            this.Nascimento = nascimento;
+            this.Cpf = cpf;
+        }
+
+
+        internal string Nome { get; set; }
+        internal string Nascimento { get; set; }
+        internal long Cpf {  get; set; }
+
+        public abstract void Info();
+    }
+
+    internal class Funcionario : Pessoa 
+    { 
+        public Funcionario() { } 
+        public Funcionario(string nome, string nascimento, long cpf, int id, string departamento) : base(nome, nascimento, cpf) 
+        { 
+            this.Id = id; 
+            this.Departameto = departamento; 
+        } 
+        public int Id { get; set; } 
+        public string Departameto { get; set; }
+
+        public override void Info()
+        {
+            Console.WriteLine($"--INFORMAÇÕES--\nNome:{Nome}\nId:{Id}\nDepartamento:{Departameto}\n");
+        }
+    }
+    internal class Tecnico : Pessoa 
+    { 
+        public Tecnico() { } 
+        public Tecnico(string nome, string nascimento, long cpf, int id, string funcao) : base(nome, nascimento, cpf)
+        {
+            this.Id = id; 
+            this.Funcao = funcao; 
+        }
+        public int Id { get; set; }
+        public string Funcao { get; set; }
+
+        public override void Info()
+        {
+            Console.WriteLine($"--INFORMAÇÕES--\nNome:{Nome}\nId:{Id}\nFunção:{Funcao}\n");
+        }
+    }
+}
---
+++ ChamadoApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using ChamadoApp;
+
+Funcionario x = new Funcionario("VArlei", "06/12/2001", 555, 1, "CPD");
+Tecnico y = new Tecnico("Joao", "06/12/2001", 555, 1, "Estagiario");
+
+x.Info();
+y.Info();
